335357bc9d65d19f625c388eaa9d7fefbdee2065
Venus Izadi <venus.izadi@gmail.com>
2015-05-20 16:31:50 +1000

    Connect the app to Firebase and Implement the edit and update functionalities

--- VenusNinja/scripts/app.js
+++ VenusNinja/scripts/app.js
@@ -18,10 +18,12 @@ var app = angular
         controller: 'TaskController'
       })
       .when('/browse',{
-        templateUrl: 'views/browse.html'
+        templateUrl: 'views/browse.html',
+        controller: 'TaskController'
       })
-      .when('/edit',{
-        templateUrl: 'views/edit.html'
+      .when('/edit/:taskId',{
+        templateUrl: 'views/edit.html',
+        controller: 'TaskController'
       })
       .otherwise({
         redirectTo: '/'
--- VenusNinja/scripts/task.js
+++ VenusNinja/scripts/task.js
@@ -1,9 +1,32 @@
 'use strict';
 
-app.controller('TaskController',function($scope){
+app.controller('TaskController',function($scope, FURL, $firebase, $location, $routeParams){
 	
-	$scope.postTask = function (task){
-		console.log(task);
+	var ref = new Firebase(FURL);
+	var fbTasks = $firebase(ref.child('tasks')).$asArray();
+
+	var taskId = $routeParams.taskId;
+
+	$scope.tasks = fbTasks;
+
+	if(taskId)
+	{
+		$scope.selectedTask = getTask(taskId);
 	}
 
+	function getTask(taskId)
+	{
+		return $firebase(ref.child('tasks').child(taskId)).$asObject();
+	}
+
+	$scope.updateTask = function(task)
+	{
+		$scope.selectedTask.$save(task);
+		$location.path('/browse');
+	}
+
+	$scope.postTask = function (task){
+		fbTasks.$add(task);
+		$location.path('/browse');
+	}
 });
